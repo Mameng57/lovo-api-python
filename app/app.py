9d1f585f15f38fcbbc2f6253fe278c802d7462f6
Mameng57 <takiya2888157@gmail.com>
2023-01-15 15:40:07 +0700

    mengubah cursor untuk menggunakan dictionary

--- app/app.py
+++ app/app.py
@@ -1,6 +1,7 @@
 from flask import Flask, Response, request, jsonify
 from mysql.connector import MySQLConnection
-from mysql.connector.cursor import MySQLCursor
+from mysql.connector.cursor import MySQLCursorDict
+from services.auth import login
 from services.user import get_all_user, get_user, create_user, update_user, delete_user
 
 
@@ -8,20 +9,22 @@ if __name__ == '__main__':
     app = Flask(__name__)
     app.config['JSON_SORT_KEYS'] = False
 
-    mysql = MySQLConnection()
-    mysql.connect(
-        host="localhost", user="root", password="", database="lovo-db"
-    )
+    mysql = MySQLConnection(host="localhost", user="root", password="", database="lovo-db")
 
     if mysql.is_connected():
         print(" * Database connected!")
 
-    cursor = MySQLCursor(mysql)
+    cursor = MySQLCursorDict(connection=mysql)
 
 
     @app.route("/", methods=["GET"])
     def test() -> Response:
         return jsonify({'status': "OK", 'message': "Terhubung dengan database!"})
+
+    @app.route("/login", methods=["POST"])
+    def login_handler() -> Response:
+        result = login(mysql, cursor, request.form)
+        return result
 
     @app.route("/user", methods=["GET", "POST"])
     def user_get_create_handler() -> Response:
@@ -32,7 +35,7 @@ if __name__ == '__main__':
         return get_all_user(cursor)
 
     @app.route("/user/<int:id>", methods=["GET", "PUT", "DELETE"])
-    def user_update_delete_handler(id: int):
+    def user_update_delete_handler(id: int) -> Response:
         if request.method == "PUT":
             result = update_user(mysql, cursor, id, request)
             return result
