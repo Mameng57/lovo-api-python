c45ed7e2565091e574642debf165ae5b85235054
Mameng57 <takiya2888157@gmail.com>
2023-01-17 16:23:51 +0700

    add photo service function

---
+++ app/services/photo.py
@@ -0,0 +1,45 @@
+if __name__ != '__main__':
+    from helpers.fetch import empty_or_row, empty_or_rows
+from flask import Response, json
+from mysql.connector.connection import MySQLCursorDict
+
+
+def get_all_session(cursor: MySQLCursorDict, id: int):
+    cursor.execute(
+        f"""
+        SELECT id_session, date, package_info, photos_count, id_user
+        FROM session WHERE id_user = {id};
+        """
+    )
+    sessions = empty_or_rows(cursor)
+
+    data = "Data Kosong..."
+
+    if sessions:
+        data = sessions
+
+    return Response(
+        mimetype="application/json",
+        status=200,
+        response=json.dumps({'status': "OK", 'data': data})
+    )
+
+
+def get_photo(cursor: MySQLCursorDict, id: int):
+    cursor.execute(
+        f"""
+        SELECT id_photo, url FROM photo WHERE id_session = {id}
+        """
+    )
+    photos = empty_or_rows(cursor)
+
+    data = "Tidak ada foto..."
+
+    if photos:
+        data = photos
+    
+    return Response(
+        mimetype="application/json",
+        status=200,
+        response=json.dumps({'status': "OK", 'data': data})
+    )
