8aadcd671b7d6f5007b6229693de90e107902c92
Mameng57 <takiya2888157@gmail.com>
2023-01-18 14:24:52 +0700

    ubah tanggal jadi tetep string biar mudah diparse

--- app/services/photo.py
+++ app/services/photo.py
@@ -2,6 +2,7 @@ if __name__ != '__main__':
     from helpers.fetch import empty_or_row, empty_or_rows
 from flask import Response, json
 from mysql.connector.connection import MySQLCursorDict
+from datetime import datetime
 
 
 def get_all_session(cursor: MySQLCursorDict, id: int):
@@ -17,11 +18,13 @@ def get_all_session(cursor: MySQLCursorDict, id: int):
 
     if sessions:
         data = sessions
+        for value in data:
+            value['date'] = str(value['date'])
 
     return Response(
         mimetype="application/json",
         status=200,
-        response=json.dumps({'status': "OK", 'data': data})
+        response=json.dumps({'status': "OK", 'session': data})
     )
 
 
@@ -41,5 +44,5 @@ def get_photo(cursor: MySQLCursorDict, id: int):
     return Response(
         mimetype="application/json",
         status=200,
-        response=json.dumps({'status': "OK", 'data': data})
+        response=json.dumps({'status': "OK", 'photo': data})
     )
